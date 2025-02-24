6acfa76dd3253abf0db6222cc3d4cdd8baab7899
brennaswilson <102930006+brennaswilson@users.noreply.github.com>
2025-02-24 02:55:00 +0000

    stdout working

--- assignment4/wilsobr2_assignment4.c
+++ assignment4/wilsobr2_assignment4.c
@@ -100,6 +100,24 @@
             close(sourceFD);
     
         }
+
+        if (curr_command->output_file != NULL){
+            int targetFD = open(curr_command->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (targetFD == -1) { 
+                perror(curr_command->output_file); 
+                fflush(stdout);
+                exit_status = 1;
+                exit(1);
+              }
+              int result = dup2(targetFD, 1);
+              if (result == -1) { 
+                perror(curr_command->output_file); 
+                exit_status = 1;
+                close(targetFD);
+                exit(1);
+            }
+            close(targetFD);
+        }
     }
 
 
@@ -142,6 +160,7 @@
      struct command_line *curr_command;
 
      int old_stdin = dup(STDIN_FILENO);
+     int old_stdout = dup(STDOUT_FILENO);
 
      while(true)
      {
@@ -194,6 +213,7 @@
             continue;
         }
         dup2(old_stdin, STDIN_FILENO);
+        dup2(old_stdout, STDIN_FILENO);
 
      }
      return EXIT_SUCCESS;
